99d737a43f150d4d8c6095a249fdd2b8d1c3f47b
mmertama <mmertama@users.noreply.github.com>
2024-03-05 14:56:29 +0000

    deploy: 3d697fb833110624ea40bf9fc1bfefb078f2f2fb

---
+++ docs/hierarchy.js
@@ -0,0 +1,9 @@
+var hierarchy =
+[
+    [ "QObject", null, [
+      [ "FigmaQmlSingleton", "classFigmaQmlSingleton.html", null ]
+    ] ],
+    [ "Qul::Singleton", null, [
+      [ "FigmaQmlSingleton", "classFigmaQmlSingleton.html", null ]
+    ] ]
+];
---
+++ docs/search/functions_8.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['valuechanged_48',['valueChanged',['../classFigmaQmlSingleton.html#aa58c3ac7b1217201d7e90284e434e085',1,'FigmaQmlSingleton']]],
+  ['view_49',['view',['../classFigmaQmlSingleton.html#a50b6370ceb6bd751edd7a41c69efc793',1,'FigmaQmlSingleton::view(int index) const'],['../classFigmaQmlSingleton.html#ae42124d245c8054e47b21ba2bdddd624',1,'FigmaQmlSingleton::view(int index) const']]],
+  ['viewcount_50',['viewCount',['../classFigmaQmlSingleton.html#a639598eb49fb005dcd17d1ef3878e50c',1,'FigmaQmlSingleton']]],
+  ['viewloaded_51',['viewLoaded',['../classFigmaQmlSingleton.html#aa36cc1d91b8d936cda511e4d051bbab1',1,'FigmaQmlSingleton']]]
+];
