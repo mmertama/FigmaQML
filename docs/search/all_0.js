cb64be7b53c1c140a065fc54bf6166400f6126de
mmertama <mmertama@users.noreply.github.com>
2024-03-05 15:10:19 +0000

    deploy: 2ed39860fa0a43dde5988fc7bd048b0e77854e52

---
+++ docs/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['applyvalue_0',['applyValue',['../classFigmaQmlSingleton.html#afbcfb8f52f64ba9fc1589ddef519404f',1,'FigmaQmlSingleton::applyValue(const QString &amp;element, const QString &amp;value)'],['../classFigmaQmlSingleton.html#abd49a4b95bedcb7c66001182da527675',1,'FigmaQmlSingleton::applyValue(SignalString element, SignalString value)']]],
+  ['arrayreceived_1',['arrayReceived',['../classFigmaQmlSingleton.html#a3b910efe043d2c1f52254e0f7760df36',1,'FigmaQmlSingleton']]]
+];
