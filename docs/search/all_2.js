dd06445f2506bdbee13c7c7d32b46411f329f82b
mmertama <mmertama@users.noreply.github.com>
2024-03-05 16:11:19 +0000

    deploy: 2f25b13cfff20be8e5170af93f9e8af1cf14cca7

---
+++ docs/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['elements_4',['elements',['../classFigmaQmlSingleton.html#a48414ee373e9396e95c46c792eec2355',1,'FigmaQmlSingleton']]],
+  ['eventreceived_5',['eventReceived',['../classFigmaQmlSingleton.html#a24702c6e2de73baf093048672d78908e',1,'FigmaQmlSingleton::eventReceived()'],['../classFigmaQmlSingleton.html#aeb330f5635bdfe8688918c921a3bc43b',1,'FigmaQmlSingleton::eventReceived()']]]
+];
---
+++ docs/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['currentview_52',['currentView',['../classFigmaQmlSingleton.html#a41f9f71a398388127cea1757b951d634',1,'FigmaQmlSingleton']]]
+];
